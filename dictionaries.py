dd85edb4125d5e00701c04d7f77b985f9cfb989b
Joël Hanskamp <36297232+joelhanskamp2@users.noreply.github.com>
2019-08-30 12:26:22 +0200

    Create dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,16 @@
+"""
+dic1 = {'0': '10', '1': '20'}
+dic1['2'] = '30'
+print ("dic1['0']: ", dic1['0'])
+print ("dic1['1']: ", dic1['1'])
+print ("dic1['2']: ", dic1['2'])
+if "0" in dic1:
+    print("0 zit erin")
+del dic1['1']
+if "1" not in dic1:
+    print("90's cranken")
+"""
+
+dic7 ={'1': '10', '2': '20'}
+dic8 ={'3': '30', '4': '40'}
+dic9 ={'5': '50', '6': '60'}
